1486553c7fd2b87f096d52e5764321bf20896068
SonCryptoz <118987900+SonCryptoz@users.noreply.github.com>
2024-09-19 23:09:07 +0700

    #4 Tạo Card Item sản phẩm

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,27 @@
-import React from "react";
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from "@ant-design/icons";
-import { Avatar, Card } from "antd";
-const { Meta } = Card;
+import classNames from "classnames/bind";
+
+import styles from "./Card.module.scss";
+
+const cx = classNames.bind(styles);
 
 function CardItem() {
-    return <div></div>;
+    return (
+        <div className={cx("item")}>
+            <div
+                className={cx("image")}
+                style={{
+                    backgroundImage: `url("https://bizweb.dktcdn.net/thumb/large/100/053/643/products/112.jpg?v=1454040113190")`,
+                }}
+            ></div>
+            <div className={cx("product-info")}>
+                <h3 className={cx("name")}>Pizza</h3>
+                <div className={cx("price")}>
+                    <span className={cx("current-price")}>200.000 ₫</span>
+                    <span className={cx("old-price")}>223.000 ₫</span>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default CardItem;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,14 @@
 import classNames from "classnames/bind";
 
 import styles from "./Home.module.scss";
+import CardItem from "@/components/Card/Card";
 
 const cx = classNames.bind(styles);
 
 function Home() {
     return (
         <div className={cx("body")}>
-            <h1>Home page</h1>
+            <CardItem />
         </div>
     );
 }
